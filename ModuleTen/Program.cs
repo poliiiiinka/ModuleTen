4766beb1ea214431cf9014e841f61038a42d47ec
Полина Артамонова <poliiiiinka71@gmail.com>
2024-05-08 16:46:32 +0300

    Добавьте файлы проекта.

---
+++ ModuleTen/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace ModuleTen
+{
+    public class Program
+    {
+        static ILogger logger { get; set; }
+        static void Main(string[] args)
+        {
+            logger = new Logger();
+            ISumCalculate sumCalculate = new SumCalculate(logger);
+
+            double a, b;
+
+            try
+            {
+                Console.WriteLine("Введите первое слагаемое:");
+                string value1 = Console.ReadLine();
+                bool isDigit1 = SumCalculate.IsDigit(value1);
+
+                Console.WriteLine("Введите второе слагаемое:");
+                string value2 = Console.ReadLine();
+                bool isDigit2 = SumCalculate.IsDigit(value2);
+
+                /// проверяем, являются ли два введённых значения числами
+                if (isDigit1 == true && isDigit2 == true)
+                {
+                    a = double.Parse(value1);
+                    b = double.Parse(value2);
+                    logger.Event("Результат вычисления:");
+                    sumCalculate.Sum(a, b);
+                }
+                else
+                {
+                    logger.Error("Не удалось получить результат!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            
+            Console.ReadKey();
+        }
+
+        public interface ILogger
+        {
+            void Event(string message);
+            void Error(string message);
+        }
+
+        public class Logger : ILogger
+        {
+            public void Error(string message)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine(message);
+            }
+
+            public void Event(string message)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
---
+++ ModuleTen/SumCalculate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ModuleTen.Program;
+
+namespace ModuleTen
+{
+    public interface ISumCalculate
+    {
+       public void Sum(double a, double b);
+    }
+
+    public class SumCalculate : ISumCalculate
+    {
+        private readonly ILogger _logger;
+
+        public SumCalculate(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Sum(double a, double b)
+        {
+            double c = a + b;
+            string sum = c.ToString();
+            Console.WriteLine(sum);
+        }
+
+        /// метод для проверки ввода числа
+        public static bool IsDigit(string value)
+        {
+            double num;
+            bool isDigit = double.TryParse(value, out num);
+            return isDigit;
+        }
+    }
+}
